889611b1fe0d1febad492548ed7b03c8056ead7d
adityavit337 <aditya.kapoor2023@vitstudent.ac.in>
2025-02-04 22:05:09 +0530

    Create q16.js

---
+++ Assignment_6/q16.js
@@ -0,0 +1,15 @@
+function bubbleSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+const inputArray = [4, 2, 9, 1, 5, 6];
+console.log(bubbleSort(inputArray));
